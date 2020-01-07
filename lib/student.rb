1907d11777c37219341a743503148582fe3f3933
yaoyu bao <yaobao5354@gmail.com>
2020-01-07 02:44:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,6 +74,19 @@ class Student
      end
   end
 
+  def self.first_students_in_grade_10(x)
+    sql = <<-SQL
+       SELECT *
+       FROM students
+       WHERE grade = 10
+       LIMIT 1
+     SQL
+
+     DB[:conn].execute(sql).map do |row|
+       self.new_from_db(row)
+     end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
